5de56e816786b6677b21cfe20285be2d6675b0c0
Sheraff <fpellet@ensc.fr>
2022-07-18 00:03:33 +0200

    add default example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import styles from './app.module.css'
 import Graph from './Graph'
 import Waveform from './Waveform'
+import example from './Graph/example'
+
+if(localStorage.length === 0) {
+	Object.entries(example).forEach(([key, value]) => {
+		localStorage.setItem(key, JSON.stringify(value))
+	})
+}
 
 function App() {
 	return (
