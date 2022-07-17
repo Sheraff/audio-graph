ced217d171b0b59666e5f5bb88550eb0837d2f56
Sheraff <fpellet@ensc.fr>
2022-07-17 17:59:39 +0200

    simpler automation by defaulting to Constant node

--- src/Graph/Player/index.jsx
+++ src/Graph/Player/index.jsx
@@ -1,96 +1,36 @@
 import { forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from "react"
 
-function getNodeParams(audioNodes, nodes, id, defaultName) {
-	const [nodeId, slotType, slotIndex] = id.split('.')
-	const node = nodes.find(node => node.id === nodeId)
-	const slot = node.slots[id]
-	const audioNode = audioNodes[nodeId]
-	if (slot.name in node.settings) {
-		if(slot.name in audioNode)
-			return {node: audioNode[slot.name], channel: 0, info: node}
-		if(audioNode.parameters?.has(slot.name))
-			return {node: audioNode.parameters.get(slot.name), channel: 0, info: node}
-	}
-	const channel = slot.name === defaultName ? 0 : Number(slotIndex)
-	return {node: audioNode, channel, info: node}
-}
-
-function plugAutomationNode(ctx, node, toParams) {
+function plugAutomationNode(ctx, node, destination) {
+	const offsetNode = destination.parameters.get('offset')
 	const duration = Number(node.settings.duration)
 	const progress = ctx.current.currentTime % duration
 	const startTime = ctx.current.currentTime - progress
 	const percent = progress / duration
-	toParams.node.cancelScheduledValues(ctx.current.currentTime)
+	offsetNode.cancelScheduledValues(ctx.current.currentTime)
 	const points = node.settings.track
 	if(!points?.length) return
 	if(points[0].x !== 0)
 		points.unshift({x: 0, y: 0})
-	if(points.at(-1).x < 0.99)
-		points.push({x: 0.99, y: points.at(-1).y})
+	if(points.at(-1).x !== 1)
+		points.push({x: 0.9999, y: points.at(-1).y})
 	{
-		const nextPoint = points.findIndex(point => point.x > percent)
+		let nextPoint = points.findIndex(point => point.x > percent)
+		if (nextPoint === -1) nextPoint = 0
 		const prevPoint = nextPoint - 1
-		const progressBetweenPrevAndNext = (percent - points[prevPoint].x) / (points[nextPoint].x - points[prevPoint].x)
-		const lerpYValue = points[prevPoint].y + (points[nextPoint].y - points[prevPoint].y) * progressBetweenPrevAndNext
-		toParams.node.setValueAtTime(lerpYValue, startTime)
+		const progressBetweenPrevAndNext = (percent - points.at(prevPoint).x) / (points.at(nextPoint).x - points.at(prevPoint).x)
+		const lerpYValue = points.at(prevPoint).y + (points.at(nextPoint).y - points.at(prevPoint).y) * progressBetweenPrevAndNext
+		offsetNode.setValueAtTime(lerpYValue, startTime)
 	}
 	for(let i = 0; i < 10_000; i++) {
 		points.forEach((point, p) => {
 			const time = startTime + (i * duration) + (point.x * duration)
 			if (time > ctx.current.currentTime) {
 				if(p === 0)
-					toParams.node.setValueAtTime(point.y, time)
+					offsetNode.setValueAtTime(point.y, time)
 				else
-					toParams.node.linearRampToValueAtTime(point.y, time)
+					offsetNode.linearRampToValueAtTime(point.y, time)
 			}
 		})
-	}
-}
-
-function plugNodeSettings(ctx, connections, audioNodes, nodes, node) {
-	if (node.type === 'automation') {
-		const connection = connections.current.list.find(connection => connection.from.split('.')[0] === node.id)
-		if (!connection) return
-		const toParams = getNodeParams(audioNodes.current, nodes.current, connection.to, 'in')
-		if (!toParams.node || toParams.channel !== 0) return
-		plugAutomationNode(ctx, node, toParams)
-	}
-
-	const audioNode = audioNodes.current[node?.id]
-	if(!audioNode) {
-		console.log('node not found', node, audioNodes.current)
-		return
-	}
-	if (node.type === 'oscillator') {
-		audioNode.type = node.settings.type
-		audioNode.frequency.value = node.settings.frequency
-		audioNode.detune.value = parseFloat(node.settings.detune)
-	} else if (node.type === 'lfo') {
-		audioNode.type = node.settings.type
-		audioNode.frequency.value = node.settings.frequency
-	} else if (node.type === 'gain') {
-		audioNode.gain.value = node.settings.gain
-	} else if (node.type === 'pan') {
-		audioNode.pan.value = node.settings.pan
-	} else if (node.type === 'delay') {
-		audioNode.delayTime.value = node.settings.delayTime
-	} else if (node.type === 'constant') {
-		audioNode.parameters.get('offset').value = parseFloat(node.settings.offset)
-	} else if (node.type === 'biquadFilter') {
-		audioNode.frequency.value = node.settings.frequency
-		audioNode.Q.value = node.settings.Q
-		audioNode.gain.value = node.settings.gain
-	} else if (node.type === 'compressor') {
-		audioNode.threshold.value = node.settings.threshold
-		audioNode.knee.value = node.settings.knee
-		audioNode.ratio.value = node.settings.ratio
-		audioNode.attack.value = node.settings.attack
-		audioNode.release.value = node.settings.release
-	} else if (node.type === 'multiplier') {
-		audioNode.parameters.get('multiplier').value = parseFloat(node.settings.multiplier)
-	} else if (node.type === 'white-noise') {
-		audioNode.parameters.get('min').value = parseFloat(node.settings.min)
-		audioNode.parameters.get('max').value = parseFloat(node.settings.max)
 	}
 }
 
@@ -113,6 +53,21 @@ function Player({nodes, connections}, ref) {
 
 	const previousConnections = useRef(new Set())
 	const updateConnections = useCallback(() => {
+		function getNodeParams(id, defaultName) {
+			const [nodeId, slotType, slotIndex] = id.split('.')
+			const node = nodes.current.find(node => node.id === nodeId)
+			const slot = node.slots[id]
+			const audioNode = audioNodes.current[nodeId]
+			if (slot.name in node.settings) {
+				if(slot.name in audioNode)
+					return {node: audioNode[slot.name], channel: 0}
+				if(audioNode.parameters?.has(slot.name))
+					return {node: audioNode.parameters.get(slot.name), channel: 0}
+			}
+			const channel = slot.name === defaultName ? 0 : Number(slotIndex)
+			return {node: audioNode, channel}
+		}
+
 		const connectionsMemo = new Set()
 		connections.current.list.forEach(connection => {
 			const {from, to} = connection
@@ -120,12 +75,8 @@ function Player({nodes, connections}, ref) {
 			const connectionId = `${from}-${to}`
 			connectionsMemo.add(connectionId)
 			if (previousConnections.current.has(connectionId)) return
-			const fromParams = getNodeParams(audioNodes.current, nodes.current, from, 'out')
-			const toParams = getNodeParams(audioNodes.current, nodes.current, to, 'in')
-			if (fromParams.info.type === 'automation' && toParams.node) {
-				plugAutomationNode(ctx, fromParams.info, toParams)
-				return
-			}
+			const fromParams = getNodeParams(from, 'out')
+			const toParams = getNodeParams(to, 'in')
 			if (!fromParams.node || !toParams.node) return
 			if (fromParams.channel === 0 && toParams.channel === 0) {
 				fromParams.node.connect(toParams.node)
@@ -137,15 +88,10 @@ function Player({nodes, connections}, ref) {
 		})
 		previousConnections.current.forEach(connectionId => {
 			if(!connectionsMemo.has(connectionId)) {
-				const [from, to] = connectionId.split('-')
-				const {node, info} = getNodeParams(audioNodes.current, nodes.current, from, 'out')
+				const [from] = connectionId.split('-')
+				const {node} = getNodeParams(from, 'out')
 				if (node) {
 					node.disconnect()
-				}
-				if (info.type === 'automation') {
-					const toParams = getNodeParams(audioNodes.current, nodes.current, to, 'in')
-					toParams.node.cancelScheduledValues(ctx.current.currentTime)
-					plugNodeSettings(ctx, connections, audioNodes, nodes, toParams.info)
 				}
 			}
 		})
@@ -153,7 +99,46 @@ function Player({nodes, connections}, ref) {
 	}, [nodes, connections])
 
 	const updateSettings = useCallback(() => {
-		nodes.current.forEach(node => plugNodeSettings(ctx, connections, audioNodes, nodes, node))
+		nodes.current.forEach(node => {
+			const audioNode = audioNodes.current[node?.id]
+			if(!audioNode) {
+				console.log('node not found', node, audioNodes.current)
+				return
+			}
+			if (node.type === 'oscillator') {
+				audioNode.type = node.settings.type
+				audioNode.frequency.value = node.settings.frequency
+				audioNode.detune.value = parseFloat(node.settings.detune)
+			} else if (node.type === 'lfo') {
+				audioNode.type = node.settings.type
+				audioNode.frequency.value = node.settings.frequency
+			} else if (node.type === 'gain') {
+				audioNode.gain.value = node.settings.gain
+			} else if (node.type === 'pan') {
+				audioNode.pan.value = node.settings.pan
+			} else if (node.type === 'delay') {
+				audioNode.delayTime.value = node.settings.delayTime
+			} else if (node.type === 'constant') {
+				audioNode.parameters.get('offset').value = parseFloat(node.settings.offset)
+			} else if (node.type === 'biquadFilter') {
+				audioNode.frequency.value = node.settings.frequency
+				audioNode.Q.value = node.settings.Q
+				audioNode.gain.value = node.settings.gain
+			} else if (node.type === 'compressor') {
+				audioNode.threshold.value = node.settings.threshold
+				audioNode.knee.value = node.settings.knee
+				audioNode.ratio.value = node.settings.ratio
+				audioNode.attack.value = node.settings.attack
+				audioNode.release.value = node.settings.release
+			} else if (node.type === 'multiplier') {
+				audioNode.parameters.get('multiplier').value = parseFloat(node.settings.multiplier)
+			} else if (node.type === 'white-noise') {
+				audioNode.parameters.get('min').value = parseFloat(node.settings.min)
+				audioNode.parameters.get('max').value = parseFloat(node.settings.max)
+			} else if (node.type === 'automation') {
+				plugAutomationNode(ctx, node, audioNode)
+			}
+		})
 	}, [nodes])
 
 	const updateNodes = useCallback(() => {
@@ -206,6 +191,9 @@ function Player({nodes, connections}, ref) {
 				audioNodes.current[node.id] = customNode
 			} else if (node.type === 'duplicate') {
 				const customNode = new AudioWorkletNode(ctx.current, 'duplicate', {numberOfOutputs: 6})
+				audioNodes.current[node.id] = customNode
+			} else if (node.type === 'automation') {
+				const customNode = new AudioWorkletNode(ctx.current, 'constant-custom', {numberOfInputs: 0, parameterData: {offset: 1}})
 				audioNodes.current[node.id] = customNode
 			} else if (node.type === 'output') {
 				audioNodes.current[node.id] = ctx.current.destination
