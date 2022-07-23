32ec095f01786fe792f3db2e8a1eeaaf021e19e8
Sheraff <fpellet@ensc.fr>
2022-07-23 20:41:04 +0200

    minor

--- src/Graph/Node/Extra/Visualizer/index.jsx
+++ src/Graph/Node/Extra/Visualizer/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useId, useRef, useState } from "react"
+import { useEffect, useId, useRef } from "react"
 import styles from "./index.module.css"
 
 const normalizeData = array => {
