f932a63d30679c3c44a330f65eaf2f8e063088d5
Flo <florian.pellet@matera.eu>
2022-07-29 13:11:58 +0200

    Random: much less math per frame

--- public/AudioWorklets/Random.js
+++ public/AudioWorklets/Random.js
@@ -8,35 +8,36 @@ class Random extends AudioWorkletProcessor {
 				defaultValue: 10,
 				minValue: 0,
 				maxValue: 100,
-				automationRate: 'a-rate'
+				automationRate: 'k-rate'
 			},
 			{
 				name: 'rate',
 				defaultValue: 60,
 				minValue: 0,
 				maxValue: 300,
-				automationRate: 'a-rate'
+				automationRate: 'k-rate'
 			},
 			{
 				name: 'min',
 				defaultValue: 0,
 				minValue: -100,
 				maxValue: 100,
-				automationRate: 'a-rate'
+				automationRate: 'k-rate'
 			},
 			{
 				name: 'max',
 				defaultValue: 1,
 				minValue: -100,
 				maxValue: 100,
-				automationRate: 'a-rate'
+				automationRate: 'k-rate'
 			},
 		]
 	}
 	constructor(options) {
 		super(options)
 		this.sampleRate = options.processorOptions.sampleRate
-		this.sinceLast = Infinity
+		this.sinceLast = 0
+		this.nextChange = 0
 		this.currentValue = 0
 	}
 	process (inputs, outputs, parameters) {
@@ -44,37 +45,25 @@ class Random extends AudioWorkletProcessor {
 		if (!base) {
 			return true
 		}
+		const rate = parameters.rate[0]
+		const variability = parameters.variability[0]
 		base.forEach((_,i) => {
 			this.sinceLast += 1 / this.sampleRate
-			changeValue: {
-				const frameRate = parameters.rate[i] ?? parameters.rate[0]
-				const frameVariability = parameters.variability[i] ?? parameters.variability[0]
+			if (this.sinceLast >= this.nextChange) {
+				const frameMin = parameters.min[0]
+				const frameMax = parameters.max[0]
+				const min = Math.min(frameMin, frameMax)
+				const max = Math.max(frameMin, frameMax)
+				this.currentValue = Math.random() * (max - min) + min
+				this.sinceLast = 0
 
-				if (frameRate === 0)
-					break changeValue
-
-				const variability = frameRate * frameVariability / 100
-				const start = 60 / (frameRate + variability)
-
-				if (this.sinceLast <= start)
-					break changeValue
-
-				let shouldChange = false
-				const end = 60 / Math.max(1, (frameRate - variability))
-				if (start === end || this.sinceLast >= end) {
-					shouldChange = true
-				} else {
-					const progress = (this.sinceLast - start) / (end - start)
-					shouldChange = Math.random() < progress**4
-				}
-				if (shouldChange) {
-					const frameMin = parameters.min[i] ?? parameters.min[0]
-					const frameMax = parameters.max[i] ?? parameters.max[0]
-					const min = Math.min(frameMin, frameMax)
-					const max = Math.max(frameMin, frameMax)
-					this.currentValue = Math.random() * (max - min) + min
-					this.sinceLast = 0
-				}
+				this.scheduleNextChange(rate, variability)
+			} else if (
+				this.lastRate !== rate
+				|| this.lastVariability !== variability
+			) {
+				this.scheduleNextChange(rate, variability)
+				this.nextChange -= this.sinceLast
 			}
 
 			outputs.forEach(output => 
@@ -84,6 +73,15 @@ class Random extends AudioWorkletProcessor {
 			)
 		})
 		return true
+	}
+
+	scheduleNextChange(rate, _variability) {
+		const variability = rate * _variability / 100
+		const start = 60 / (rate + variability)
+		const end = 60 / Math.max(1, (rate - variability))
+		this.nextChange = Math.random() * (end - start) + start
+		this.lastRate = rate
+		this.lastVariability = _variability
 	}
 }
 
