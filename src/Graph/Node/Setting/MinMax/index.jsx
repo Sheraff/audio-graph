fa24d17d57887bc7a38323e8491b92073d312452
Sheraff <fpellet@ensc.fr>
2022-07-30 14:03:50 +0200

    MinMax input type fully working

--- src/Graph/Node/Setting/MinMax/index.jsx
+++ src/Graph/Node/Setting/MinMax/index.jsx
@@ -1,14 +1,15 @@
 import classNames from 'classnames'
-import { useEffect, useRef, memo } from 'react'
+import React, { useEffect, useRef, memo } from 'react'
 import styles from './index.module.css'
 
-function MinMax({name, defaultValue, props, instance}){
+function MinMax({name, defaultValue, props}){
 	const mainRef = useRef(/** @type {HTMLDivElement} */(null))
-	const inputRef = useRef(/** @type {HTMLInput} */(null))
+	const inputRef = useRef(/** @type {HTMLInputElement} */(null))
+	const minInputRef = useRef(/** @type {HTMLInputElement} */(null))
+	const maxInputRef = useRef(/** @type {HTMLInputElement} */(null))
 	const trackRef = useRef(/** @type {HTMLDivElement} */(null))
 	const minRef = useRef(/** @type {HTMLDivElement} */(null))
 	const maxRef = useRef(/** @type {HTMLDivElement} */(null))
-	console.log('------ rerender')
 
 	useEffect(() => {
 		const controller = new AbortController()
@@ -18,15 +19,15 @@ function MinMax({name, defaultValue, props, instance}){
 		const updateDisplay = () => {
 			mainRef.current.style.setProperty('--min', (values[0] - props.min) / delta)
 			mainRef.current.style.setProperty('--max', (values[1] - props.min) / delta)
+			minInputRef.current.value = values[0]
+			maxInputRef.current.value = values[1]
+			minInputRef.current.setAttribute('max', values[1])
+			maxInputRef.current.setAttribute('min', values[0])
 		}
 		updateDisplay()
 
 		Object.defineProperty(inputRef.current, 'values', {
 			get: () => values,
-			set: (v) => {
-				values = v
-				updateDisplay()
-			},
 			configurable: true,
 		})
 
@@ -34,16 +35,30 @@ function MinMax({name, defaultValue, props, instance}){
 			const {clientX} = event
 			const {left, width} = mainRef.current.getBoundingClientRect()
 			const x = clientX - left
-			return Math.min(props.max, Math.max(props.min, x / width * delta + props.min))
+			const value = Math.min(props.max, Math.max(props.min, x / width * delta + props.min))
+			const clampedToStep = Math.round(value / props.step) * props.step
+			return clampedToStep
 		}
 
-		let grabbing = false
+		/** @type {null | HTMLDivElement} */
+		let grabbing = null
+
 		minRef.current.addEventListener('mousedown', (event) => {
 			grabbing = event.target
 		}, {signal: controller.signal})
 		maxRef.current.addEventListener('mousedown', (event) => {
 			grabbing = event.target
 		}, {signal: controller.signal})
+
+		const processValue = (value) => {
+			const valueIndex = grabbing === minRef.current ? 0 : 1
+			if (values[valueIndex] !== value) {
+				values[valueIndex] = value
+				updateDisplay()
+				inputRef.current.dispatchEvent(new Event('input', {bubbles: true}))
+			}
+		}
+
 		trackRef.current.addEventListener('mousedown', (event) => {
 			const value = eventToValue(event)
 			if (Math.abs(value - values[0]) < Math.abs(value - values[1])) {
@@ -51,9 +66,7 @@ function MinMax({name, defaultValue, props, instance}){
 			} else {
 				grabbing = maxRef.current
 			}
-			values[grabbing === minRef.current ? 0 : 1] = value
-			updateDisplay()
-			inputRef.current.dispatchEvent(new Event('input'))
+			processValue(value)
 		}, {signal: controller.signal})
 
 		window.addEventListener('mousemove', (event) => {
@@ -62,60 +75,99 @@ function MinMax({name, defaultValue, props, instance}){
 			let value = eventToValue(event)
 			if (grabbing === minRef.current && value > values[1]) value = values[1]
 			if (grabbing === maxRef.current && value < values[0]) value = values[0]
-			values[grabbing === minRef.current ? 0 : 1] = value
-			updateDisplay()
-			inputRef.current.dispatchEvent(new Event('input'))
+			processValue(value)
 		}, {signal: controller.signal})
 
 		window.addEventListener('mouseup', (e) => {
 			if(!grabbing) return
-			grabbing = false
-			inputRef.current.dispatchEvent(new Event('change'))
+			grabbing = null
+			inputRef.current.dispatchEvent(new Event('change', {bubbles: true}))
 		}, {signal: controller.signal})
+
+		const onKeyChange = (index, value) => {
+			if (values[index] !== value) {
+				values[index] = value
+				updateDisplay()
+				inputRef.current.dispatchEvent(new Event('input', {bubbles: true}))
+				inputRef.current.dispatchEvent(new Event('change', {bubbles: true}))
+			}
+		}
 
 		minRef.current.addEventListener('keydown', (event) => {
 			if (event.key === 'ArrowLeft' || event.key === 'ArrowDown') {
-				values[0] = Math.max(props.min, values[0] - props.step)
-				updateDisplay()
+				const value = Math.max(props.min, values[0] - props.step)
+				onKeyChange(0, value)
 				event.preventDefault()
-				inputRef.current.dispatchEvent(new Event('change'))
-				inputRef.current.dispatchEvent(new Event('input'))
-			}
-			if (event.key === 'ArrowRight' || event.key === 'ArrowUp') {
-				values[0] = Math.min(values[1], values[0] + props.step)
-				updateDisplay()
+			} else if (event.key === 'ArrowRight' || event.key === 'ArrowUp') {
+				const value = Math.min(values[1], values[0] + props.step)
+				onKeyChange(0, value)
 				event.preventDefault()
-				inputRef.current.dispatchEvent(new Event('change'))
-				inputRef.current.dispatchEvent(new Event('input'))
 			}
 		}, {signal: controller.signal})
 		maxRef.current.addEventListener('keydown', (event) => {
 			if (event.key === 'ArrowLeft' || event.key === 'ArrowDown') {
-				values[1] = Math.max(values[0], values[1] - props.step)
-				updateDisplay()
+				const value = Math.max(values[0], values[1] - props.step)
+				onKeyChange(1, value)
 				event.preventDefault()
-				inputRef.current.dispatchEvent(new Event('change'))
-				inputRef.current.dispatchEvent(new Event('input'))
-			}
-			if (event.key === 'ArrowRight' || event.key === 'ArrowUp') {
-				values[1] = Math.min(props.max, values[1] + props.step)
-				updateDisplay()
+			} else if (event.key === 'ArrowRight' || event.key === 'ArrowUp') {
+				const value = Math.min(props.max, values[1] + props.step)
+				onKeyChange(1, value)
 				event.preventDefault()
-				inputRef.current.dispatchEvent(new Event('change'))
-				inputRef.current.dispatchEvent(new Event('input'))
 			}
 		}, {signal: controller.signal})
 
+		function numberInputEvent(element, type, index) {
+			element.addEventListener(type, (event) => {
+				values[index] = event.target.value
+				updateDisplay()
+				inputRef.current.dispatchEvent(new Event(type, {bubbles: true}))
+			}, {signal: controller.signal})
+		}
+		numberInputEvent(minInputRef.current, 'input', 0)
+		numberInputEvent(minInputRef.current, 'change', 0)
+		numberInputEvent(maxInputRef.current, 'input', 1)
+		numberInputEvent(maxInputRef.current, 'change', 1)
+
 		return () => controller.abort()
-	}, [props.min, props.max, props.step, defaultValue[0], defaultValue[1]])
+	}, [
+		props.min,
+		props.max,
+		props.step,
+		defaultValue[0],
+		defaultValue[1],
+	])
 
 	return (
 		<>
-			<span className={styles.main} ref={mainRef}>
+			<span className={styles.main} ref={mainRef} style={{
+				'--min': (defaultValue[0] - props.min) / (props.max - props.min),
+				'--max': (defaultValue[1] - props.min) / (props.max - props.min),
+			}}>
 				<input className={styles.input} ref={inputRef} name={name} />
-				<span ref={minRef} className={classNames(styles.thumb, styles.min)} tabIndex="0" />
-				<span ref={maxRef} className={classNames(styles.thumb, styles.max)} tabIndex="0" />
+				<span ref={minRef} className={classNames(styles.thumb, styles.min)} tabIndex={0} />
+				<span ref={maxRef} className={classNames(styles.thumb, styles.max)} tabIndex={0} />
 				<span ref={trackRef} className={styles.track}/>
+			</span>
+			<span>
+				<input
+					ref={minInputRef}
+					type="number"
+					className={styles.text}
+					step={props.step}
+					min={props.min}
+					max={props.max}
+					defaultValue={defaultValue[0]}
+				/>
+				:
+				<input
+					ref={maxInputRef}
+					type="number"
+					className={styles.text}
+					step={props.step}
+					min={props.min}
+					max={props.max}
+					defaultValue={defaultValue[1]}
+				/>
 			</span>
 		</>
 	)
